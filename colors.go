e7a2cbcc3c6bdcef07a6aee9eea978d9e4214dce
wsxiaoys <wsxiaoys.lh@gmail.com>
2011-11-24 14:59:26 +0800

    Use new color syntax

--- colors.go
+++ colors.go
@@ -1,96 +1,137 @@
 package colors
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 )
 
-const (
-	// Attributes
-	ATTR_RESET     = 0
-	ATTR_BRIGHT    = 1
-	ATTR_DIM       = 2
-	ATTR_UNDERLINE = 3
-	ATTR_BLINK     = 5
-	ATTR_REVERSE   = 7
+const escapeCode = '@'
 
-	// Foreground
-	FG_BLACK   = 30
-	FG_RED     = 31
-	FG_GREEN   = 32
-	FG_YELLOW  = 33
-	FG_BLUE    = 34
-	FG_MAGENTA = 35
-	FG_CYAN    = 36
-	FG_WHITE   = 37
-	FG_DEFAULT = 39
+const resetCode = "@0"
 
-	// Background
-	BG_BLACK   = 40
-	BG_RED     = 41
-	BG_GREEN   = 42
-	BG_YELLOW  = 43
-	BG_BLUE    = 44
-	BG_MAGENTA = 45
-	BG_CYAN    = 46
-	BG_WHITE   = 47
-	BG_DEFAULT = 49
-)
+var codeMap = map[int]int{
+	'0': 0,
+	'!': 1,
+	'.': 2,
+	'_': 3,
+	'?': 5,
+	'/': 7,
+	'-': 8,
 
-var (
-	Default = Color{ATTR_RESET, FG_DEFAULT, BG_DEFAULT}
-	Black   = Color{ATTR_RESET, FG_BLACK, BG_DEFAULT}
-	Red     = Color{ATTR_RESET, FG_RED, BG_DEFAULT}
-	Green   = Color{ATTR_RESET, FG_GREEN, BG_DEFAULT}
-	Yellow  = Color{ATTR_RESET, FG_YELLOW, BG_DEFAULT}
-	Blue    = Color{ATTR_RESET, FG_BLUE, BG_DEFAULT}
-	Magenta = Color{ATTR_RESET, FG_MAGENTA, BG_DEFAULT}
-	Cyan    = Color{ATTR_RESET, FG_CYAN, BG_DEFAULT}
-	White   = Color{ATTR_RESET, FG_WHITE, BG_DEFAULT}
-)
+	'r': 31,
+	'g': 32,
+	'y': 33,
+	'b': 34,
+	'm': 35,
+	'c': 36,
+	'w': 37,
+	'd': 39,
 
-type Color struct {
-	attr, fg, bg uint8
+	'R': 41,
+	'G': 42,
+	'Y': 43,
+	'B': 44,
+	'M': 45,
+	'C': 46,
+	'W': 47,
+	'D': 49,
 }
 
-func (x Color) Format(f fmt.State, c rune) {
-	if c == 'C' || c == 'v' {
-		str := fmt.Sprint("\033[", x.attr, ";", x.fg, ";", x.bg, "m")
-		bytes := []byte(str)
-		f.Write(bytes)
-	} else {
-		f.Write([]byte(fmt.Sprint("#!C(colors.Color)")))
+func colorMap(x string) string {
+	attr := 0
+	fg := 39
+	bg := 49
+
+	for _, key := range x {
+		c, ok := codeMap[key]
+		switch {
+		case !ok:
+			log.Fatalf("Wrong color syntax: %c", key)
+		case 0 <= c && c <= 8:
+			attr = c
+		case 30 <= c && c <= 37:
+			fg = c
+		case 40 <= c && c <= 47:
+			bg = c
+		}
+	}
+	return fmt.Sprintf("\033[%d;%d;%dm", attr, fg, bg)
+}
+
+func compileColorSyntax(input, output *bytes.Buffer) {
+	i, _, err := input.ReadRune()
+	if err != nil {
+		// EOF got
+		log.Fatal("Parse failed on color syntax")
+	}
+
+	switch i {
+	default:
+		output.WriteString(colorMap(string(i)))
+	case '{':
+		color := bytes.NewBufferString("")
+		for {
+			i, _, err := input.ReadRune()
+			if err != nil {
+				log.Fatal("Parse failed on color syntax")
+			}
+			if i == '}' {
+				break
+			}
+			color.WriteRune(i)
+		}
+		output.WriteString(colorMap(color.String()))
+	case escapeCode:
+		output.WriteRune(escapeCode)
 	}
 }
 
-func (x Color) Print(a ...interface{}) (n int, err error) {
-	fmt.Print(x)
-	n, err = fmt.Print(a...)
-	fmt.Print(Default)
-	return
+func compile(x string) string {
+	if x == "" {
+		return ""
+	}
+
+	input := bytes.NewBufferString(x)
+	output := bytes.NewBufferString("")
+
+	for {
+		i, _, err := input.ReadRune()
+		if err != nil {
+			break
+		}
+		switch i {
+		default:
+			output.WriteRune(i)
+		case escapeCode:
+			compileColorSyntax(input, output)
+		}
+	}
+	return output.String()
 }
 
-func (x Color) Println(a ...interface{}) (n int, err error) {
-	fmt.Print(x)
-	n, err = fmt.Println(a...)
-	fmt.Print(Default)
-	return
+func compileValues(a *[]interface{}) {
+	for i, x := range *a {
+                if str, ok := x.(string); ok {
+			(*a)[i] = compile(str)
+                }
+	}
 }
 
-func (x Color) Printf(format string, a ...interface{}) (n int, err error) {
-	fmt.Print(x)
-	n, err = fmt.Printf(format, a...)
-	fmt.Print(Default)
-	return
+func Print(a ...interface{}) (int, error) {
+	a = append(a, resetCode)
+	compileValues(&a)
+	return fmt.Print(a...)
 }
 
-func Cprint(a ...interface{}) (n int, err error) {
-	n, err = fmt.Print(a...)
-	fmt.Print(Default)
-	return
+func Println(a ...interface{}) (int, error) {
+	a = append(a, resetCode)
+	compileValues(&a)
+	return fmt.Println(a...)
 }
 
-func Cprintf(format string, a ...interface{}) (n int, err error) {
-	n, err = fmt.Printf(format, a...)
-	fmt.Print(Default)
-	return
+func Printf(format string, a ...interface{}) (int, error) {
+	format += resetCode
+	format = compile(format)
+	return fmt.Printf(format, a...)
 }
